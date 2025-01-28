fae1590ffe865256c325e18cdc55d7733fa57baa
YanxiangShan <leoshanyx@outlook.com>
2025-01-27 19:01:54 -0500

    new mlr data

--- code/MLR/analysis.R
+++ code/MLR/analysis.R
@@ -4,44 +4,50 @@ processed.data <- read.csv("./processed_data.csv")
 medal.colnames <- c("Bronze", "Silver", "Gold", "Total")
 drop.cols <- c(medal.colnames, "Rank", "Year", "NOC")
 
-total.mdl  <- lm(Total  ~ ., data = processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Total"))], na.action = na.omit)
-gold.mdl   <- lm(Gold   ~ ., data = processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Gold"))])
-silver.mdl <- lm(Silver ~ ., data = processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Silver"))])
-bronze.mdl <- lm(Bronze ~ ., data = processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Bronze"))])
+bronze.mdl <- lm(Bronze ~ . + Is.host*Events + Is.host*Mean.Event.Participation, data = processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Bronze"))])
+silver.mdl <- lm(Silver ~ . + Is.host*Events + Is.host*Mean.Event.Participation, data = processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Silver"))])
+gold.mdl   <- lm(Gold   ~ . + Is.host*Events + Is.host*Mean.Event.Participation, data = processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Gold"))])
+total.mdl  <- lm(Total  ~ . + Is.host*Events + Is.host*Mean.Event.Participation, data = processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Total"))])
 
-summary(total.mdl)
+# Now do backwards selection
+library(MASS)
+
+bronze.red <- step(bronze.mdl)
+silver.red <- step(silver.mdl)
+gold.red   <- step(gold.mdl)
+total.red  <- step(total.mdl)
 
 # Extreme in Y
 alpha <- 0.05
-n <- nobs(total.mdl)
-p <- length(coef(total.mdl))
+n <- nobs(total.red)
+p <- length(coef(total.red))
 
 processed.data.total <- processed.data[, !(names(processed.data) %in% setdiff(drop.cols, "Total"))]
-y.extremes <- processed.data.total[abs(rstudent(total.mdl)) >= qt(1 - alpha/n, n - p - 1), ]
+y.extremes <- processed.data.total[abs(rstudent(total.red)) >= qt(1 - alpha/n, n - p - 1), ]
 nrow(y.extremes)
 
 # Extreme in X
-x.extremes <- processed.data.total[hatvalues(total.mdl) >= 2*p, ]
+x.extremes <- processed.data.total[hatvalues(total.red) >= 2*p, ]
 nrow(x.extremes)
 
 ##  Influential points
 # DFFITS
-dffits.ex <- processed.data[abs(dffits(total.mdl)) >= 1, ] # DFFITS, cutoff |DFFITS| >= 1
+dffits.ex <- processed.data[abs(dffits(total.red)) >= 1, ] # DFFITS, cutoff |DFFITS| >= 1
 nrow(dffits.ex)
 write.csv(dffits.ex[, c("NOC", "Year")], "./analysis/DFFITS.csv", row.names = FALSE)
 
 # Cook's Distance D, cutoff |D| >= F(0.5, p, n - p)
-cd.ex <- processed.data[cooks.distance(total.mdl) >= qf(0.5, p, n - p), ] # Cook's D
+cd.ex <- processed.data[cooks.distance(total.red) >= qf(0.5, p, n - p), ] # Cook's D
 nrow(cd.ex)
 write.csv(cd.ex[, c("NOC", "Year")], "./analysis/cooks_distance.csv", row.names = FALSE)
 
 # DFBETAS, cutoff |DFBETAS| >= 1
-dfbetas.ex <- processed.data.total[abs(dfbetas(total.mdl)) >= 1]
+dfbetas.ex <- processed.data.total[abs(dfbetas(total.red)) >= 1]
 length(dfbetas.ex)
 
 all.values <- data.frame()
 
-preds <- names(coef(total.mdl))
+preds <- names(coef(total.red))
 preds <- setdiff(preds, "(Intercept)")
 p.values <- rep(0, length(preds))
 names(p.values) <- preds
@@ -50,7 +56,7 @@ for (pred.name in preds) {
   pred <- na.omit(unlist(processed.data[[pred.name]], use.names = FALSE))
   med <- median(pred)
   # Next, we calculate the values needed to get $s^2$ and $t^*$
-  res <- resid(total.mdl)
+  res <- resid(total.red)
   e1 <- na.omit(res[pred <= med])
   e2 <- na.omit(res[pred > med])
   d1 <- abs(e1 - median(e1))
@@ -69,11 +75,6 @@ for (pred.name in preds) {
 
 all.values <- rbind(all.values, p.values)
 
-# Generate latex tables
-library(stargazer)
-
-stargazer(bronze.mdl, silver.mdl, gold.mdl, total.mdl, type='latex')
-
 total.slr.sports <- lm(Total ~ Sports, data = processed.data)
 total.slr.events <- lm(Total ~ Events, data = processed.data)
 
@@ -82,10 +83,14 @@ stargazer(total.slr.sports, total.slr.events,
 
 library(car)
 
-VIFs <- cbind(vif(bronze.mdl), vif(silver.mdl), vif(gold.mdl), vif(total.mdl))
-colnames(VIFs) <- medal.colnames
-stargazer(vif(total.mdl), type='latex', flip = TRUE)
+vif(total.red)
 
-VIF.diffs <- VIF - vif(total.mdl)
+step(total.red)
+
+VIFs <- cbind(vif(bronze.red), vif(silver.red), vif(gold.red), vif(total.red))
+colnames(VIFs) <- medal.colnames
+stargazer(vif(total.red), type='latex', flip = TRUE)
+
+VIF.diffs <- VIF - vif(total.red)
 write.csv(VIFs, "./analysis/vifs.csv", row.names = FALSE)
 write.csv(VIF.diffs, "./analysis/vif_diffs.csv", row.names = FALSE)
